12322d724ed9737489ce5fd5ac79422df3f8859b
Brandon Simanteris <brandon@b26.io>
2025-12-24 10:42:20 -0500

    removed fontawesome from public view

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { PUBLIC_FONTAWESOME_KIT_URL } from '$env/static/public';
 	
 	let { children } = $props();
 </script>
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+	{#if PUBLIC_FONTAWESOME_KIT_URL}
+		<script src={PUBLIC_FONTAWESOME_KIT_URL} crossorigin="anonymous"></script>
+	{/if}
 </svelte:head>
 
 {@render children()}
